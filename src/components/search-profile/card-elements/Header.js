ca891afa79b6a287c496e164133a33e8e79b8be2
sumukha.m <sumukhamohan6@gmail.com>
2022-10-15 14:26:39 +0530

    fix(searchProfile): input filter is cleared on close of selected option

--- src/components/search-profile/card-elements/Header.js
+++ src/components/search-profile/card-elements/Header.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SelectedOption from "../../common/SelectedOption";
 import TextField from "../../common/TextField";
 
 const Header = (props) => {
   const { getFilterVal, setSelectedProfile, selectedProfile } = props;
   const onInput = (event) => getFilterVal(event.target.value);
+
+  useEffect(() => {
+    getFilterVal("");
+  }, [selectedProfile]);
 
   return (
     <div>
@@ -16,7 +20,13 @@ const Header = (props) => {
           className="plain-text-input"
         />
       ) : (
-        <div><SelectedOption label={selectedProfile.name} className="select-optn" onClose={()=> setSelectedProfile(null)} /></div>
+        <div>
+          <SelectedOption
+            label={selectedProfile.name}
+            className="select-optn"
+            onClose={() => setSelectedProfile(null)}
+          />
+        </div>
       )}
     </div>
   );
