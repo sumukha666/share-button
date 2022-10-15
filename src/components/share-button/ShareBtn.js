e650a837478837732bc7e23eff8f65447706a889
sumukha.m <sumukhamohan6@gmail.com>
2022-10-15 11:41:54 +0530

    feat(shareBtn): added logic to conditionally render ShareCard

--- src/components/share-button/ShareBtn.js
+++ src/components/share-button/ShareBtn.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../common/Button";
+import ShareCard from "./ShareCard";
 
 const ShareBtn = () => {
+  const [showCard, setShowCard] = useState(false);
+  const toggleShare = () => setShowCard(!showCard);
+
   return (
     <>
-      <Button label="Share" />
+      <Button label="Share" onClick={toggleShare} />
+      {showCard && <ShareCard />}
     </>
   );
 };
