3fa24cc0571ce4b299dd3f3f8038127942c0aa48
sumukha.m <sumukhamohan6@gmail.com>
2022-10-15 14:47:11 +0530

    feat(searchField): added prop to disable the input field

--- src/components/common/SearchField.js
+++ src/components/common/SearchField.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 const SearchField = (props) => {
-  const { onClick, name, placeholder, btnLabel, onInputClick } = props;
+  const { onClick, name, placeholder, btnLabel, onInputClick, disabled } =
+    props;
   return (
     <div className="dsp-flex">
-      <input
-        className="search-input"
-        name={name}
-        placeholder={placeholder}
-        onClick={onInputClick}
-      ></input>
+      <div onClick={onInputClick} className="wd-100-per">
+        <input
+          className="search-input"
+          name={name}
+          placeholder={placeholder}
+          disabled={disabled}
+        ></input>
+      </div>
       <button onClick={onClick} className="search-btn">
         {btnLabel}
       </button>
